e714fab97f3631141e481df1cfa9cd05ced5cad4
Mildred Zaragoza <mildredzarv1@gmail.com>
2022-01-26 14:52:06 -0600

    main image modified

--- src/App/AnyTask/index.js
+++ src/App/AnyTask/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './AnyTask.css';
-import noTask from '../images/new-task.png';
+/* import noTask from '../images/new-task.png'; */
 
 function AnyTask(){
     return(
         <>
-            <img id='movement' src={noTask} alt = 'Task image'/>
+          {/*   <img id='movement' src={noTask} alt = 'Task image'/> */}
+            <div id= 'movement'></div>
             <h3>Oh, you've got nothing!</h3>
             <p>Use the button below to add new tasks</p>
         </>
